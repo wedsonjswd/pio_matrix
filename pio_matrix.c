e5f3a14b903050e5dbd7b2f3935c87a625227b04
wedson.jswd <wedson.silva@univasf.edu.br>
2025-01-27 16:24:47 -0300

    "Funçõa para mostrar as figuras em uma úica cor"

--- pio_matrix.c
+++ pio_matrix.c
@@ -35,46 +35,46 @@ double desenho2[25] =   {1.0, 0.0, 0.0, 0.0, 1.0,
                         1.0, 0.0, 0.0, 0.0, 1.0};
 
 //imprimir valor binário
-void imprimir_binario(int num) {
- int i;
- for (i = 31; i >= 0; i--) {
-  (num & (1 << i)) ? printf("1") : printf("0");
- }
+void imprimir_binario(int num) 
+{
+    int i;
+    for (i = 31; i >= 0; i--) 
+    {
+        (num & (1 << i)) ? printf("1") : printf("0");
+    }
 }
 
 //rotina da interrupção
-static void gpio_irq_handler(uint gpio, uint32_t events){
+static void gpio_irq_handler(uint gpio, uint32_t events)
+{
     printf("Interrupção ocorreu no pino %d, no evento %d\n", gpio, events);
     printf("HABILITANDO O MODO GRAVAÇÃO");
-	reset_usb_boot(0,0); //habilita o modo de gravação do microcontrolador
+	
+    reset_usb_boot(0,0); //habilita o modo de gravação do microcontrolador
 }
 
 //rotina para definição da intensidade de cores do led
 uint32_t matrix_rgb(double b, double r, double g)
 {
-  unsigned char R, G, B;
-  R = r * 255;
-  G = g * 255;
-  B = b * 255;
-  return (G << 24) | (R << 16) | (B << 8);
+    unsigned char R, G, B;
+    R = r * 255;
+    G = g * 255;
+    B = b * 255;
+    return (G << 24) | (R << 16) | (B << 8);
 }
 
 //rotina para acionar a matrix de leds - ws2812b
-void desenho_pio(double *desenho, uint32_t valor_led, PIO pio, uint sm, double r, double g, double b){
-
-    for (int16_t i = 0; i < NUM_PIXELS; i++) {
-        if (i%2==0)
-        {
-            valor_led = matrix_rgb(desenho[24-i], r=0.0, g=0.0);
-            pio_sm_put_blocking(pio, sm, valor_led);
-
-        }else{
-            valor_led = matrix_rgb(b=0.0, desenho[24-i], g=0.0);
-            pio_sm_put_blocking(pio, sm, valor_led);
-        }
+void desenho_pio(double *desenho, uint32_t valor_led, PIO pio, uint sm, double r, double g, double b)
+{
+    for (int16_t i = 0; i < NUM_PIXELS; i++) 
+    {
+        valor_led = matrix_rgb(desenho[24 - i], 0.0, 0.0); // Azul apenas
+        pio_sm_put_blocking(pio, sm, valor_led);
     }
+    
     imprimir_binario(valor_led);
 }
+
 
 //função principal
 int main()
@@ -112,21 +112,23 @@ int main()
     //interrupção da gpio habilitada
     gpio_set_irq_enabled_with_callback(button_0, GPIO_IRQ_EDGE_FALL, 1, & gpio_irq_handler);
 
-    while (true) {
+    while (true) 
+    {
+        if(gpio_get(button_1)) //botão em nível alto
+        {
+            //rotina para escrever na matriz de leds com o emprego de PIO - desenho 2
+            desenho_pio(desenho, valor_led, pio, sm, r, g, b);
+            printf("\nfrequeência de clock %ld\r\n", clock_get_hz(clk_sys));
+        }
+        else
+        {
+            //rotina para escrever na matriz de leds com o emprego de PIO - desenho 1
+            desenho_pio(desenho2, valor_led, pio, sm, r, g, b);
+            printf("\nfrequeência de clock %ld\r\n", clock_get_hz(clk_sys));
+        }
     
-    if(gpio_get(button_1)) //botão em nível alto
-    {
-        //rotina para escrever na matriz de leds com o emprego de PIO - desenho 2
-        desenho_pio(desenho, valor_led, pio, sm, r, g, b);
-    }
-    else
-    {
-        //rotina para escrever na matriz de leds com o emprego de PIO - desenho 1
-        desenho_pio(desenho2, valor_led, pio, sm, r, g, b);
-    }
-
-    sleep_ms(500);
-    printf("\nfrequeência de clock %ld\r\n", clock_get_hz(clk_sys));
+        sleep_ms(1000);
+        printf("\nfrequeência de clock %ld\r\n", clock_get_hz(clk_sys));
     }
     
 }
